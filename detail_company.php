8a7941b62a7887aaea996e5405b31d664d303d40
arthityaBB <artitaya014041@gmail.com>
2025-05-04 22:40:40 +0700

    A

--- detail_company.php
+++ detail_company.php
@@ -18,7 +18,7 @@ $company = mysqli_fetch_assoc($result);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>รายละเอียดบริษัท</title>
     <link rel="icon" href="images/Logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
